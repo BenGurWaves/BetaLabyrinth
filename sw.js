74eff168abdc81c324ba252de80453a9756f8a5a
BenGurWaves <bengurwaves@gmail.com>
2026-01-06 11:26:21 -0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'labyrinth-v0.5.4';
+const ASSETS = [
+  '/',
+  '/chats.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  // Add icons paths
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/icon-maskable-192.png',
+  '/icons/icon-maskable-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Future push notifications placeholder
+self.addEventListener('push', event => {
+  console.log('Push received');
+  // Add notification show when ready
+});
